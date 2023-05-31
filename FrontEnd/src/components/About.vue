aa8a8c08b63670c0dd0c9a9e83c2a63bfb31845e
Jesus Berdugo <berdugopertuz@gmail.com>
2023-05-31 18:11:21 -0500

    jesus/Seccion cuatro terminada

---
+++ FrontEnd/src/components/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <article class="w-1/2 h-full">
+      <h2 class="text-[48px] font-bold text-center mt-4 mx-4 mb-4 border-t-2 border-b-2 border-black">NOSOTROS</h2>
+      <p class="text-[22px] text-justify mx-4">
+        Lorem ipsum dolor sit amet consectetur. Dictum lectus sed neque arcu auctor aenean est. Proin tortor sit tincidunt viverra lorem. Eu proin pharetra malesuada sed ut ornare. Tellus scelerisque mattis enim quis sit convallis in. Amet scelerisque vulputate placerat dis. Iaculis est ipsum purus tempor venenatis. Dolor arcu arcu egestas mauris purus scelerisque pellentesque nisi. In diam eget quis aliquet tellus netus porta convallis. Eget ornare ultrices nunc nulla tellus id posuere adipiscing. Enim morbi dictumst elit facilisis porttitor. Amet morbi lacus enim feugiat. Eu vitae sollicitudin in sed elementum morbi aenean. Aliquam.
+      </p>
+    </article>
+    <article class="w-1/2 h-full">
+      <div class="bg-[url('../public/img/img-3.jpg')] h-1/2 border-2 border-black p-4">
+        <div class="border h-full p-4 flex items-end">
+          <span class="text-[32px] text-white">Frase para destacar <br> la experiencia en el rubro</span>
+        </div>
+      </div>
+      <div class="bg-[url('../public/img/img-2.jpg')] h-1/2 border-x-2 border-b-2 border-black p-4">
+        <div class="border h-full p-4 flex items-end">
+          <span class="text-[32px] text-white">Frase para destacar <br> diferencia entre paihuen y otros</span>
+        </div>
+      </div>
+    </article>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+</style>
---
+++ FrontEnd/src/components/Contactos.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="flex h-full bg-terceario">
+    <div class="flex justify-center items-center w-[33.5%] px-20">
+      <p class="text-[24px] text-justify text-white">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur facilis temporibus
+        natus totam beatae eligendi ipsa cum alias sint asperiores aliquam dolorem blanditiis et
+        voluptatem, sapiente, ipsam itaque. Quasi, unde.
+      </p>
+    </div>
+    <div class="w-[33.5%] p-4">
+      <form class="flex flex-col gap-2 bg-[#28DB24] h-full p-8 rounded-xl" action="#">
+        <span class="text-white text-lg">
+          Entreganos los datos relevantes para tu consulta y te responderemos lo antes posible
+        </span>
+        <input class="py-2 px-4 rounded-lg bg-slate-50" type="text" placeholder="Nombre..." />
+        <input class="py-2 px-4 rounded-lg bg-slate-50" type="email" placeholder="Email..." />
+        <input class="py-2 px-4 rounded-lg bg-slate-50" type="text" placeholder="Telefono..." />
+        <textarea
+          class="py-2 px-4 rounded-lg bg-slate-50"
+          cols="30"
+          rows="3"
+          placeholder="...que buscas?"
+        ></textarea>
+        <button class="self-start bg-black text-white py-2 px-6 rounded-3xl mt-4">Enviar</button>
+      </form>
+    </div>
+    <div class="w-[33.5%] flex flex-col items-center pt-8">
+      <h3 class="text-[36px] text-white border-b-2 w-[450px] text-center">
+        Información de Contacto
+      </h3>
+      <div class="flex flex-col gap-5 text-[22px] mt-6 text-white">
+        <span><i class="fa-solid fa-location-dot"></i> Calle Nombre 123, santiago, chile</span>
+        <span><i class="fa-solid fa-phone"></i> Calle Nombre 123, santiago, chile</span>
+        <span><i class="fa-solid fa-phone"></i> Calle Nombre 123, santiago, chile</span>
+        <span><i class="fa-solid fa-envelope"></i> Calle Nombre 123, santiago, chile</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped></style>
